1116e9e650f6cce2be7657eaa4265a20afecf515
Williams Almeida <williannsbrown@gmail.com>
2023-11-08 21:40:30 -0300

    fix: adjust the order of elements

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/app/components/skeleton'
 
 export default function HomeLoading() {
   return (
-    <div className="grid-rows-6 grid h-full grid-cols-9 gap-6">
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
       <Skeleton className="col-span-6 row-span-6 h-[856px]" />
 
       <Skeleton className="col-span-3 row-span-3" />
