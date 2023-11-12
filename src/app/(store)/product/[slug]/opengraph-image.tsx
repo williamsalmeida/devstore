b4a0668db6e5170b7a1ec38bd9ebd012559f4e1b
Williams Almeida <williannsbrown@gmail.com>
2023-11-12 10:48:42 -0300

    feat: add opengraph-image component and update env schema

---
+++ src/app/(store)/product/[slug]/opengraph-image.tsx
@@ -0,0 +1,51 @@
+import { api } from '@/data/api'
+import { Product } from '@/data/types/products'
+import { env } from '@/env'
+import { ImageResponse } from 'next/og'
+import { zinc } from 'tailwindcss/colors'
+
+export const runtime = 'edge'
+
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 15, // 15 minutes
+    },
+  })
+
+  const product = await response.json()
+
+  return product
+}
+
+export default async function Image({ params }: { params: { slug: string } }) {
+  const product = await getProduct(params.slug)
+
+  const productImageURL = new URL(product.image, env.APP_URL).toString()
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          background: zinc[950],
+        }}
+      >
+        <img src={productImageURL} alt="" style={{ width: '100%' }} />
+      </div>
+    ),
+    {
+      ...size,
+    },
+  )
+}
