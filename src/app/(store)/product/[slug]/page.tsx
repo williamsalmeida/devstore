9cf86df9190edcf8e4a4fde05e268a6153a0e4ce
Williams Almeida <williannsbrown@gmail.com>
2023-11-12 11:38:03 -0300

    feat: create AddToCartButton component to ProductPage

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { AddToCartButton } from '@/app/components/add-to-cart-button'
 import { api } from '@/data/api'
 import { Product } from '@/data/types/products'
 import { Metadata } from 'next'
@@ -104,12 +105,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </span>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
