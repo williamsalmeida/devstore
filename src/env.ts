7dac2c0e6b53a697bc3c00ac00a13b02b358ad27
Williams Almeida <williannsbrown@gmail.com>
2023-11-15 12:06:16 -0300

    Revert "chore: add client/server env variables boundaries"
    This reverts commit a85530503e2e6bdd55769d4b84f880726f28b11f.

--- src/env.ts
+++ src/env.ts
@@ -1,16 +1,19 @@
-import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
-export const env = createEnv({
-  server: {
-    APP_URL: z.string().url(),
-  },
-  client: {
-    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-  },
-
-  runtimeEnv: {
-    APP_URL: process.env.APP_URL,
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  },
+const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  APP_URL: z.string().url(),
 })
+
+const parsedEnv = envSchema.safeParse(process.env)
+
+if (!parsedEnv.success) {
+  console.error(
+    'Invalid environment variable',
+    parsedEnv.error.flatten().fieldErrors,
+  )
+
+  throw new Error('Invalid environment variable')
+}
+
+export const env = parsedEnv.data
